739cc77fde68479da8bf18fde2ba67b038e07e95
Marek Generowicz <generowicz@gmail.com>
2022-02-04 16:24:07 +0200

    Add files via upload

--- config/configuration.nix
+++ config/configuration.nix
@@ -2,17 +2,15 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-
 { config, pkgs, ... }:
- 
-{    
+
+{
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-
-  nix = {
+nix = {
     package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
     extraOptions = ''
       experimental-features = nix-command flakes
@@ -20,29 +18,31 @@
    };
 
 
-  # Allow instalation of proprietary or unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_s>
+
+   #Host Name ~ WLAN ~ LAN
+  
+   networking.hostName = "nixos-laptop"; # Define your hostname.
+   
+ # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
    networking.wireless.userControlled.enable = true;
+   
+   networking.useDHCP = false; 
+   networking.interfaces.enp3s0.useDHCP = true;
+   networking.interfaces.wlp2s0.useDHCP = true;
+   networking.networkmanager.enable = true;
+
+
 
   # Set your time zone.
    time.timeZone = "Europe/Bucharest";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to fals>
-  # Per-interface useDHCP will be mandatory in the future, so this generate>
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.interfaces.wlp2s0.useDHCP = true;
-  networking.networkmanager.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -54,20 +54,19 @@
   #   keyMap = "us";
   # };
 
-     # Enable the X11 windowing system.
-   services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-   # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
 
-   # Enable the KDE Desktop Environment.
-   services.xserver.displayManager.sddm.enable    = true;
-   services.xserver.desktopManager.plasma5.enable = true;
-
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
 
   # Configure keymap in X11
    services.xserver.layout = "us";
    services.xserver.xkbOptions = "eurosign:e";
+
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -80,18 +79,19 @@
    services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.generowicz = {
+     users.users.generowicz = {
      isNormalUser = true;
      extraGroups = [ "wheel""networkmanager" ]; # Enable ‘sudo’ for the user.
    };
-
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+   nixpkgs.config.allowUnfree = true;
+   
    environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Na>
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      firefox
-
    ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -100,10 +100,6 @@
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
-
-
-}
-
   # };
 
   # List services that you want to enable:
@@ -123,5 +119,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  # system.stateVersion = "21.11"; # Did you read the comment?
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
 
